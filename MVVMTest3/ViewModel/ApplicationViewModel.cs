51af49f4c098a0c949c187b15e34fc5a7be99de7
Puncker1978 <nvv197827@gmail.com>
2023-08-22 22:44:05 +0500

    Добавьте файлы проекта.

---
+++ MVVMTest3/ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MVVMTest3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMTest3.ViewModel
+{
+    public class ApplicationViewModel
+    {
+        public ICommand OpenNewClientCommand { get; private set; }
+
+        #region Метод открытия окна для добавления нового клиента
+        #endregion
+
+        #region Команда открытия окна для добавления нового клиента
+        #endregion
+    }
+}
---
+++ MVVMTest3/ViewModel/PersonViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MVVMTest2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMTest3.ViewModel
+{
+    public class PersonViewModel
+    {
+        public ICommand SaveNewClientCommand { get; private set; }
+
+        #region Метод сохранения нового клиента в репозитории клиентов
+        #endregion
+
+        #region Команда сохранения нового клиента в репозитории клиентов
+        #endregion
+    }
+}
